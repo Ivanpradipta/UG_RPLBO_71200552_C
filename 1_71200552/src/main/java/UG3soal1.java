2e2b119f8151cf8edb3b2b715a9eaba2e33f0c70
Ivanpradipta <79517923+Ivanpradipta@users.noreply.github.com>
2022-02-23 17:26:16 +0700

    Add files via upload

---
+++ 1_71200552/src/main/java/UG3soal1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class UG3soal1 {
+    public static void main(String[] args){
+
+        String nama,tanggal;
+        int jumlah;
+        float berat;
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Data Product");
+
+        System.out.println("Masukan data product makanan anda");
+        System.out.print("Nama makanan: ");
+        nama = keyboard.nextLine();
+
+        System.out.print("Tanggal kadaluarsa: ");
+        tanggal = keyboard.nextLine();
+
+        System.out.print("jumlah(qty): ");
+        jumlah= keyboard.nextInt();
+
+        System.out.print("berat: ");
+        berat= keyboard.nextInt();
+
+        System.out.println("Data Product");
+        System.out.println("Nama Makanan: " + nama);
+        System.out.println("Tanggal kadaluarsa: " + tanggal);
+        System.out.println("jumlah qty: " +jumlah);
+        System.out.println("Berat(g): " + berat +" "+"gram");
+
+
+    }
+
+}
