ccca5edac70e9facc9d792046d78c526f06b1cd0
Ivanpradipta <79517923+Ivanpradipta@users.noreply.github.com>
2022-03-16 18:41:44 +0700

    Add files via upload

---
+++ UG6_71200552/71200552-UG6-1/src/main/java/com/ug6/soal1/Vending_Machines.java
@@ -0,0 +1,47 @@
+package com.ug6.soal1;
+
+import java.util.ArrayList;
+
+public class Vending_Machines {
+    private String code;
+    private int capacity;
+    private int usedCapacity;
+    private ArrayList<Goods> goods;
+    private ArrayList<double> acceptanceBalance;
+    private double consumeMoney;
+
+
+    public void VendingMachines(){
+
+    }
+
+    public void VendingMachines(int code, int capacity){
+        this.code=code;
+        this.capacity=capacity
+    }
+
+    public VendingMachine(int code, int capacity, Goods goods, double acceptanceBalance){
+
+    }
+
+    public void proceedOrder(String goodsCode, int orderQuantity){
+
+    }
+
+    public  void takeMoney(double money){
+
+    }
+
+    public void giveGood(double goodPrice, String goodsName){
+
+    }
+
+    public double withdrawMoney(){
+
+    }
+
+    public void  inputGoods(Goods good){
+
+    }
+
+}
