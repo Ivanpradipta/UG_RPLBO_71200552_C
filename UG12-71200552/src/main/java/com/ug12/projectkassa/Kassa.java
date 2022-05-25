7e43e708b1b11933dac9a7af3b72004121ef886e
Ivanpradipta <79517923+Ivanpradipta@users.noreply.github.com>
2022-05-25 18:07:46 +0700

    Add files via upload

---
+++ UG12-71200552/src/main/java/com/ug12/projectkassa/Kassa.java
@@ -0,0 +1,6 @@
+package com.ug12.projectkassa;
+
+public class Kassa {
+    public void register(Kasir kasir1) {
+    }
+}
---
+++ UG12-71200552/src/main/java/com/ug12/projectkassa/Produk.java
@@ -0,0 +1,4 @@
+package com.ug12.projectkassa;
+
+public class Produk {
+}
